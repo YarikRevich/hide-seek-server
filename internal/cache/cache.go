a1bc2e17aaa6419cc486db5f3b4463770e2f7250
Yarik Svitlitskiy <yariksvitlitskiy81@gmail.com>
2022-01-15 11:59:50 +0200

    fix: fixed cache

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -14,12 +14,17 @@ type Cache struct {
 
 func (c *Cache) start() {
 	go func() {
-		for range c.ticker.C {
-			for k, v := range c.cache {
-				if time.Since(v) == 0 {
-					// collection.UseCollection().CleanDataByUUID(k)
-					delete(c.cache, k)
+		spain := time.NewTicker(time.Millisecond * 10)
+		for {
+			select {
+			case <-c.ticker.C:
+				for k, v := range c.cache {
+					if time.Since(v) == 0 {
+						// collection.UseCollection().CleanDataByUUID(k)
+						delete(c.cache, k)
+					}
 				}
+			case <-spain.C:
 			}
 		}
 	}()
